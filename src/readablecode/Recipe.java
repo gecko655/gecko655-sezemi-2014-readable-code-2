1f8d4e4a29a8ad88afc5c3d1b8049d0c7b0ba967
gecko655 <aqwsedrft1234@yahoo.co.jp>
2014-07-06 11:20:20 +0900

    spec3: Implementing with hard coding of recipe-data.txt

--- src/readablecode/Recipe.java
+++ src/readablecode/Recipe.java
@@ -1,10 +1,31 @@
 package readablecode;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Recipe {
 
 
     public static void main(String[] args) {
-        System.out.println("オムライス");
+        File file = new File("recipe-data.txt");
+        if(!file.exists()){
+            System.err.println("File does not exist. Stop.");
+            return;
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            System.out.println(reader.readLine());
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
